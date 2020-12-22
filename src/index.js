db443fb2bc29aa52e51c4b2d70321d87db406889
Jirffey Gabuya <boyhimas@gmail.com>
2020-12-22 18:11:30 +0100

    add logic to generate random nodes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
-console.log('hello')
+import jQuery from 'jquery'
+
+const getRandomColor = () => {
+  const letters = '0123456789ABCDEF'
+  let color = '#'
+
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * letters.length)]
+  }
+
+  return color
+}
+
+const getRandomPosition = (min, max) => Math.random() * (max - min) + min
+
+function generateRandomBlock({
+  $, // jQuery object
+  $parent,
+  color,
+  position,
+}) {
+  const $el = $('<div></div>')
+    .css({
+      background: color,
+      top: position.top,
+      left: position.left,
+    })
+    .addClass('node')
+    .hide()
+
+  return $parent.append($el.fadeIn(500))
+}
+
+;(function ($) {
+  // cache DOM selectors
+  const $window = $(window)
+  const $wrapper = $('#wrapper')
+
+  const width = $window.width()
+  const height = $window.height()
+
+  setInterval(function () {
+    const position = {
+      top: getRandomPosition(0, height),
+      left: getRandomPosition(0, width),
+    }
+
+    generateRandomBlock({
+      $,
+      $parent: $wrapper,
+      color: getRandomColor(),
+      position,
+    })
+  }, 100)
+})(jQuery)
